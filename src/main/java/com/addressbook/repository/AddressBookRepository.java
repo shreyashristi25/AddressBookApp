bf8522273597fa92cf6b9ff952b398aeb0bef105
shreyashristi25 <shreyashristi25@gmail.com>
2026-03-08 20:00:13 +0530

    [Shreya] : Updated feature/UC2-add-contact

--- src/main/java/com/addressbook/repository/AddressBookRepository.java
+++ src/main/java/com/addressbook/repository/AddressBookRepository.java
@@ -1,5 +1,21 @@
 package com.addressbook.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+import com.addressbook.model.Contact;
+
+@Repository
 public class AddressBookRepository {
 
+    private List<Contact> contacts = new ArrayList<>();
+
+    public void addContact(Contact contact) {
+        contacts.add(contact);
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
 }
